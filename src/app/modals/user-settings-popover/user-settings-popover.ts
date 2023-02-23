3ec46bbb57c00309e7d24dc08fe232a8fd7bd35f
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2023-02-23 12:29:07 +0200

    Fix user settings popover

--- src/app/modals/user-settings-popover/user-settings-popover.ts
+++ src/app/modals/user-settings-popover/user-settings-popover.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { ModalController } from '@ionic/angular';
+import { PopoverController } from '@ionic/angular';
 import { EventsService } from 'src/app/services/events/events.service';
 import { ConfigService } from 'src/app/services/config/core/config.service';
 import { UserSettingsService } from 'src/app/services/settings/user-settings.service';
@@ -20,37 +20,18 @@ import { LanguageService } from 'src/app/services/languages/language.service';
   styleUrls: ['user-settings-popover.scss']
 })
 export class UserSettingsPopoverPage {
-  readToggles: {
-      'comments': boolean,
-      'personInfo': boolean,
-      'placeInfo': boolean,
-      'changes': boolean,
-      'abbreviations': boolean,
-      'pageNumbering': boolean
-  };
-
-  show = {
-      'comments': false,
-      'personInfo': false,
-      'placeInfo': false,
-      'changes': false,
-      'abbreviations': false,
-      'pageNumbering': false
-  };
 
   enableLanguageChanges = true;
-  fontsize = null;
   language?: string;
+
   constructor(
-    public viewCtrl: ModalController,
+    public viewCtrl: PopoverController,
     private config: ConfigService,
     public userSettingsService: UserSettingsService,
     public languageService: LanguageService,
     private events: EventsService
   ) {
-    this.readToggles = this.config.getSettings('settings.readToggles') as any;
-    this.enableLanguageChanges = this.config.getSettings('i18n.enableLanguageChanges') as any;
-    // this.show = readPopoverService.show;
+    this.enableLanguageChanges = this.config.getSettings('i18n.enableLanguageChanges') as boolean;
   }
 
   ionViewWillLeave() {
@@ -64,19 +45,4 @@ export class UserSettingsPopoverPage {
     this.viewCtrl.dismiss();
   }
 
-  toggleComments() {
-    // this.readPopoverService.show.comments = this.show.comments;
-  }
-
-  togglePersonInfo() {
-    // this.readPopoverService.show.personInfo = this.show.personInfo;
-  }
-
-  toggleAbbreviations() {
-    // this.readPopoverService.show.abbreviations = this.show.abbreviations;
-  }
-
-  togglePageNumbering() {
-    // git this.readPopoverService.show.pageNumbering = this.show.pageNumbering;
-  }
 }
