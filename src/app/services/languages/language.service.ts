724d192a63556d5eebcac9e9364b8708ecf4a8cf
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2023-02-28 12:24:44 +0200

    Fix language service

--- src/app/services/languages/language.service.ts
+++ src/app/services/languages/language.service.ts
@@ -11,6 +11,8 @@ export class LanguageService {
 
   private _language: string | null = null;
   private _langChangeEnabled: boolean;
+  private _availableLanguages: Array<string>;
+  private _defaultLanguage: string;
   private languageSubject: BehaviorSubject<string>;
 
   constructor(
@@ -19,24 +21,41 @@ export class LanguageService {
     private config: ConfigService,
     private events: EventsService
   ) {
-    translate.addLangs(this.config.getSettings('i18n.languages') as Array<string>);
-    translate.setDefaultLang(this.config.getSettings('i18n.locale') as string);
+    console.log('initializing language service');
 
     this._langChangeEnabled = this.config.getSettings('i18n.enableLanguageChanges') as boolean;
+    this._defaultLanguage = this.config.getSettings('i18n.locale') as string;
+    this._availableLanguages = this.config.getSettings('i18n.languages') as Array<string>;
+    this.translate.addLangs(this._availableLanguages);
+    this.translate.setDefaultLang(this._defaultLanguage);
 
-    this.languageSubject = new BehaviorSubject<string>(translate.currentLang);
+    if (!this._langChangeEnabled) {
+      this.translate.use(this._defaultLanguage);
+    } else {
+      this.storage.get('language').then((lang) => {
+        if (lang) {
+          this.translate.use(lang);
+        } else {
+          const browserLang = this.translate.getBrowserLang();
+          if (browserLang && this._availableLanguages.includes(browserLang)) {
+            this.translate.use(browserLang);
+          } else {
+            this.translate.use(this._defaultLanguage);
+          }
+        }
+      }).catch(() => {
+        this.translate.use(this._defaultLanguage);
+      });
+    }
 
-    this.getLanguage().subscribe((lang: string) => {
-      // console.log('initializing language service, lang:', lang);
-      this.setLanguage(lang);
-      this.storage.set('language', translate.currentLang);
-      this._language = translate.currentLang;
-    });
+    this._language = this.translate.currentLang;
+    this.storage.set('language', this.translate.currentLang);
+    this.languageSubject = new BehaviorSubject<string>(this.translate.currentLang);
 
-    translate.onLangChange.subscribe((event: LangChangeEvent) => {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
       const prevLang = this._language;
-      this.storage.set('language', translate.currentLang);
-      this._language = translate.currentLang;
+      this.storage.set('language', this.translate.currentLang);
+      this._language = this.translate.currentLang;
       if (this._language !== undefined && prevLang !== this._language) {
         this.updateLanguageSubject(this._language);
       }
@@ -48,9 +67,9 @@ export class LanguageService {
     const storage = this.storage;
     const _language = this._language;
     const that = this as any;
-    return Observable.create(function (subscriber: any) {
+    return new Observable(function (subscriber: any) {
       if (!that._langChangeEnabled) {
-        subscriber.next(translate.getDefaultLang());
+        subscriber.next(that._defaultLanguage);
         subscriber.complete();
         return;
       }
@@ -67,12 +86,11 @@ export class LanguageService {
           subscriber.complete();
         } else {
           const browserLang = translate.getBrowserLang();
-          const availableLanguages: string[] = that.config.getSettings('i18n.languages');
-          subscriber.next(browserLang?.match(availableLanguages.join('|')) ? browserLang : translate.getDefaultLang())
+          subscriber.next(browserLang?.match(that._availableLanguages.join('|')) ? browserLang : that._defaultLanguage)
           subscriber.complete();
         }
       }).catch(() => {
-          that.lang = that.config.getSettings('i18n.locale');
+          that.lang = that._defaultLanguage;
           subscriber.next(that.lang);
           subscriber.complete();
       });
@@ -80,12 +98,12 @@ export class LanguageService {
   }
 
   public setLanguage(lang: string) {
-    this.translate.use(lang).subscribe(
-      res => {
+    this.translate.use(lang).subscribe({
+      next: res => {
         this.events.publishLanguageStaticChange();
-        },
-        err => console.error(err)
-    );
+      },
+      error: err => console.error(err)
+    });
   }
 
   public get(text: string) {
@@ -97,7 +115,10 @@ export class LanguageService {
   }
 
   set language(lang: string) {
-    // validate that it is in supported languages...
+    // Validate that lang is in supported languages
+    if (!this._availableLanguages.includes(lang)) {
+      lang = this._defaultLanguage;
+    }
     this.setLanguage(lang);
   }
 
